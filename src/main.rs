490dede7e00eac94db271c8f116480b22aae2666
Adimac93 <Adimac93@gmail.com>
2022-07-14 15:17:04 +0200

    block

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use macroquad::prelude::*;
 
+const BLOCK_SIZE: Vec2 = const_vec2!([100f32, 40f32]);
 const PLAYER_SIZE: Vec2 = const_vec2!([150f32, 40f32]);
 const PLAYER_SPEED: f32 = 700f32;
 
@@ -32,6 +33,21 @@ impl Player {
         }
         if self.rect.x > screen_width() - self.rect.w {
             self.rect.x = screen_width() - self.rect.w;
+        }
+    }
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE)
+    }
+}
+
+struct Block {
+    rect: Rect,
+}
+
+impl Block {
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y),
         }
     }
     pub fn draw(&self) {
